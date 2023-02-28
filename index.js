79d6c574851701624fea339c113c1feabd42fc08
Muhammed Sinan KP <muhammed.sinan@neutrinos.co>
2023-02-28 11:52:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const dfff = require('dialogflow-fulfillment')
+const app = express();
+app.get('/', (req, res) => {
+    res.send("it's working")
+})
+
+app.post('/', (req, res) => {
+    const agent = new dfff.WebhookClient({
+        request: req,
+        response: res
+    })
+
+    function demo(agent){
+        agent.add("Sending response from webhook server")
+    }
+    
+    let intentMap = new Map()
+
+    intentMap.set('rent_apartment',demo)
+
+    agent.handleRequest(intentMap)
+})
+
+app.listen(3333, () => console.log('server running at port 3333'))
