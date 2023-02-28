9092aa3cab6354565f6d265794f1f3fc66c91a6c
Muhammed Sinan KP <muhammed.sinan@neutrinos.co>
2023-02-28 17:40:36 +0530

    console fixed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/',express.json(), (req, res) => {
     })
 
     function demo(agent){
-        console.log(agent)
+        console.log(typeof agent)
         agent.add("Sending response from webhook server")
     }
     
